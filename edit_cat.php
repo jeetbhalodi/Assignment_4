85afc052a10bacaf70ac73fc90651fa8f84ca4a6
jeet bhalodi <pateljeet840@gmail.com>
2021-08-11 15:41:00 +0530

    final touch

--- edit_cat.php
+++ edit_cat.php
@@ -1,11 +1,13 @@
 <?php
     include 'connection.php';
-    $id = $_GET['id_cat'];
-    $cat_name = $_GET['cat_update'];
+    $id = $_POST['id_cat'];
+    $cat_name = $_POST['cat_update'];
     
 
-    $sql = "UPDATE category SET category_name = $cat_name WHERE cat_id = $id";
+    $sql = "UPDATE category SET category_name = '$cat_name' WHERE cat_id = '$id'";
     $result = $conn->query($sql);
+
+
 
     header("Location: home.php")
 ?>
